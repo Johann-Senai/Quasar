b79cf13bdde8070554943b251fa3202b644559c9
Johann-Senai <125502913+Johann-Senai@users.noreply.github.com>
2023-03-08 17:16:22 -0300

    update
    still shit. migh fix, idk.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-$(function() {
-  $('#nav-search-select').change(function() {
-    var selectedText = $(this).find('option:selected').text();
-    $('#nav-search').find('.nav-search-label').html(selectedText);
-  });
+// get dropdown button and dropdown content
+var dropdownBtn = document.getElementsByClassName("dropbtn")[0];
+var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+
+// add click event listener to dropdown button
+dropdownBtn.addEventListener("click", function() {
+  // toggle show class on dropdown content
+  dropdownContent.classList.toggle("show");
 });
