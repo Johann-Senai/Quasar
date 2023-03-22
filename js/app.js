567f3afa103d44e51df1691462a91ad51bd7314a
Johann-Senai <125502913+Johann-Senai@users.noreply.github.com>
2023-03-22 13:53:27 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// get dropdown button and dropdown content
-var dropdownBtn = document.getElementsByClassName("dropbtn")[0];
-var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
-
-// add click event listener to dropdown button
-dropdownBtn.addEventListener("click", function() {
-  // toggle show class on dropdown content
-  dropdownContent.classList.toggle("show");
-});
